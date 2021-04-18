ac0f25bd2823f811f34b4886de2d0a5d357e087a
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-04-18 12:35:43 -0700

    LIFT

---
+++ the-lift-main/Lift/Entities/Building.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Lift.Enums;
+
+namespace Lift.Entities
+{
+    public class Building
+    {
+        public Floor[] Floors { get; set; }
+
+        public Lift Lift { get; set; }
+
+        public Building(int liftCapacity, int[][] floorAndPeopleComposition)
+        {
+            Floors = floorAndPeopleComposition.Select((floorComposition, floorNumber) =>
+            {
+                var floor = new Floor(floorNumber, floorComposition);
+                floor.ButtonPressedForCallingTheLift += this.LiftRequested;
+                return floor;
+            }).ToArray();
+
+            Lift = new Lift(liftCapacity);
+        }
+
+        public void LiftRequested(Direction direction, int floorNumberRequestedOn)
+        {
+            if(direction == Direction.GoingUp)
+            {
+                Lift.MoveUp();
+            }
+            if(direction == Direction.GoingDown)
+            {
+                Lift.MoveDown();
+            }
+        }
+    }
+}
---
+++ the-lift-main/Lift/Entities/Lift.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lift.Entities
+{
+    public class Lift
+    {
+        public int Capacity { get; set; }
+        public List<Person> People { get; set; }
+        public int CurrentFloor { get; set; }
+
+        public Lift(int capacity)
+        {
+            this.CurrentFloor = 0;
+            this.Capacity = capacity;
+        }
+
+        public static void MoveUp()
+        {
+            if (this.CurrentFloor < Building.Floors.Length())
+            {
+                this.CurrentFloor = this.CurrentFloor + 1;
+            }
+        }
+
+        public static void MoveDown()
+        {
+            if(CurrentFloor > 0)
+            {
+                this.CurrentFloor = this.CurrentFloor - 1;
+
+            }
+        }
+
+        public bool IsFull()
+        {
+            if(this.People.Count == this.Capacity)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
